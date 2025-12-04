78eed63ffe977717d4ffeb00618e43e48020abdd
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-12-04 09:35:18 +0200

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,26 @@
+﻿namespace ForLoopEmdless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop endless");
+
+            //kasutae for loopi
+            ushort i = 0;
+
+            // kui on 10 rida konsoolis, siislõpetab ära
+            //VIHJE: kasutage break-1
+
+            for (; ; )
+            {
+                Console.WriteLine("i väärtus on: {0}", i);
+                i++;
+                if (i > 10)               
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
