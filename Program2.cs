6c955d6a423a77c0e192cc2791514a24766bef43
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-12-04 09:02:46 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿namespace ForLoopEmdless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop endless");
+
+            //kasutae for loopi
+            ushort i = 0;
+
+            for (; ; )
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
